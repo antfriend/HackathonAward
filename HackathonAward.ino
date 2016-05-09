bb70d80562478f86b1ad4eea2a33c0fb13da5a80
danray <dray@healthwise.org>
2016-05-09 17:02:20 -0600

    blarg

--- HackathonAward.ino
+++ HackathonAward.ino
@@ -4,8 +4,19 @@ Servo servoX;
 Servo servoY;  
 
 int pos = 0;
-int msDelay = 55;
 
+const int PhotoBottomPin = A0;
+const int PhotoRightPin = A1;
+const int PhotoLeftPin = A2;
+
+int msDelay = 55;
+int PhotoBottom = 0;
+int PhotoRight = 0;
+int PhotoLeft = 0;
+int xDiff = 0;
+int yDiff = 0;
+int servoXPos = 0;
+int servoYPos = 0;
 
 void setup() {
   servoX.attach(10);  
@@ -13,6 +24,32 @@ void setup() {
 }
 
 void loop() {
+  getLEDvalues();
+  //servoXPos = map(sensorValue, 0, 1023, 0, 180);
+  
+  if(PhotoLeft >= PhotoRight)
+  {
+    xDiff = PhotoLeft - PhotoRight;
+    servoXPos = 90 + xDiff;
+    servoX.write(servoXPos);
+    
+  }else{
+    xDiff = PhotoRight - PhotoLeft;
+    servoXPos = 90 - xDiff;
+    servoX.write(servoXPos);
+    
+  }
+}
+
+void getLEDvalues(){
+  delay(10);// delay in between reads for stability
+  PhotoBottom = analogRead(PhotoBottomPin);
+  PhotoRight = analogRead(PhotoRightPin);
+  PhotoLeft = analogRead(PhotoLeftPin);
+}
+
+void movementTestLoop()
+{
   for (pos = 0; pos <= 180; pos += 1) { 
     // in steps of 1 degree
     servoX.write(pos);     
@@ -25,4 +62,6 @@ void loop() {
     delay(msDelay);             
   }
 }
+
+
 
